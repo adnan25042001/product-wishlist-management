3997366e68842fd632436f82c8718457458fad3b
Adnan <adnan.hussain.136660@gmail.com>
2024-02-17 11:55:21 +0530

    Initial Commit

---
+++ src/main/java/com/wishlist/ProductWishlistManagementApplication.java
@@ -0,0 +1,13 @@
+package com.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductWishlistManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductWishlistManagementApplication.class, args);
+	}
+
+}
